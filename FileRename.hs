a3952b47d46d26d262e0181e4a2c7e903d438f80
Jesper Axelsson <jesperaxe@gmail.com>
2011-05-24 21:51:22 +0200

    Moved regx and tstFile to FileRename.hs
    Main compiles now..

--- FileRename.hs
+++ FileRename.hs
@@ -5,14 +5,15 @@ import System.FilePath
 import System.Directory as Dir
 
 regx = "([0-9]+)[^0-9]+([0-9]+)"  
+tstFile =  "foo_123_456.txt"
 
-rename from to s = Dir.renameFile s (replaceBaseName s newName)
+regRenameFile from to s = Dir.renameFile s (regReplaceBaseName from to s)
+
+regReplaceBaseName from to s = (replaceBaseName s newName)
     where baseName = takeBaseName s
           newName = (substitute from to baseName)
---        ext = takeExtension s
 
 substitute from to s = makeValid $ (subRegex (mkRegex from) s to)
 
 filterExt ext files = filter (\f -> takeExtension f == ext) files
 
-tstFile =  "foo_123_456.txt"
--- Main.hs
+++ Main.hs
@@ -3,9 +3,12 @@ module Main where
 import DirectoryListing
 import FileRename
 
-regx = "([0-9]+)[^0-9]+([0-9]+)"  
+--regx = "([0-9]+)[^0-9]+([0-9]+)"  
 
 main = do
-    files <- getFiles ""
-    mapM_ putStrLn $ map (renameFile regx "\\2 \\1") (filterExt ".txt" files)
+    files <- getFiles path
+    let fFiles = (filterExt "png" files)
+    mapM_ putStrLn files
+--    mapM_ putStrLn  (regReplaceBaseName regx "\\1 \\2") fFiles
+--    mapM_ (rename regx "\\2 \\1") (filterExt ".txt" files)
     
